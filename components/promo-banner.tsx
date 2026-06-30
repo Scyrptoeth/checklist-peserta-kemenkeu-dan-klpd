f6a7d27832a7aeabc91ad1f42d3584ee14abc6b1
Scyrptoeth <sarvarra10@gmail.com>
2026-06-30 07:27:08 +0700

    Update promo-banner.tsx

--- components/promo-banner.tsx
+++ components/promo-banner.tsx
@@ -19,7 +19,7 @@ export function PromoBanner() {
             Daftar Kelas Gelombang Terakhir Sekarang!
           </span>
           <span className="text-xs font-bold text-red-600 sm:text-sm">
-            Kuota Tersisa: 11!
+            Kuota Tersisa: 5!
           </span>
           <span className="inline-flex items-center gap-0.5 text-[10px] font-semibold tracking-wide text-brand-700 underline decoration-brand-500 underline-offset-2 transition-colors group-hover:text-brand-800 sm:text-xs">
             Klik s.id/tubelstan
